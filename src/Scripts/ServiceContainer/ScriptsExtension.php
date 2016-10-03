3c3e729abde84b5c46e2edbb63479ac125a5cbde
James Kirby <jedkirby@users.noreply.github.com>
2016-10-03 13:59:18 +0100

    Script recursion detection (#29)

--- src/Scripts/ServiceContainer/ScriptsExtension.php
+++ src/Scripts/ServiceContainer/ScriptsExtension.php
@@ -77,21 +77,18 @@ class ScriptsExtension implements ExtensionInterface
         ->end();
     }
 
-    public function hasInfiniteRecursion(array $scripts, $parentName = null)
+    public function hasInfiniteRecursion(array $scripts)
     {
         foreach ($scripts as $name => $commands) {
             foreach ($commands as $command) {
                 if (strpos($command, '@') === 0) {
                     $command = substr($command, 1);
-                    if ($command === $parentName) {
+                    if ($command === $name) {
                         return true;
                     }
-
-                    return $this->hasInfiniteRecursion($scripts[$command]);
                 }
             }
         }
-
         return false;
     }
 
