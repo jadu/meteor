19b9cbc32e2091893edcf69c9deaafaa547731e0
Tom Graham <tom.graham@jadu.co.uk>
2016-12-28 11:45:35 +0000

    Removed commented out code

--- tests/Patch/Manifest/ManifestCheckerTest.php
+++ tests/Patch/Manifest/ManifestCheckerTest.php
@@ -66,44 +66,4 @@ class ManifestCheckerTest extends \PHPUnit_Framework_TestCase
 
         $this->assertTrue($this->manifestChecker->check(vfsStream::url('root')));
     }
-
-    // /**
-    //  * @param string $workingDir
-    //  */
-    // public function check($workingDir)
-    // {
-    //     $manifestFile = $workingDir . '/' . self::MANIFEST_FILENAME;
-    //     if (!file_exists($manifestFile)) {
-    //         // The manifest did not exist
-    //         return false;
-    //     }
-
-    //     $manifest = json_decode(file_get_contents($manifestFile));
-    //     if (!$manifest) {
-    //         $this->io->error('The package manifest could not be read.');
-
-    //         return false;
-    //     }
-
-    //     $errors = [];
-    //     foreach ($manifest as $path => $hash) {
-    //         $workingPath = $workingDir . '/' . $path;
-    //         if (!file_exists($workingPath)) {
-    //             $errors[] = sprintf('"%s" does not exist', $path);
-    //         }
-
-    //         if (!hash_equals($hash, sha1_file($workingPath))) {
-    //             $errors[] = sprintf('"%s" does not match the expected file contents', $path);
-    //         }
-    //     }
-
-    //     if (!empty($errors)) {
-    //         $this->io->error('The patch cannot be applied as the package could not be verified against the manifest.');
-    //         $this->io->listing($errors);
-
-    //         return false;
-    //     }
-
-    //     return true;
-    // }
 }
